70922616eeaf543685d3ee1fde6a3f234f6e6d0b
Gabriel Miroiu <miroiugabriel1231@gmail.com>
2021-04-14 22:13:10 +0300

    +

--- src/middlewares/getIpInfo.ts
+++ src/middlewares/getIpInfo.ts
@@ -2,9 +2,6 @@ import { Request, Response } from 'express';
 import geoip from 'geoip-lite';
 
 const getIpInfo = (ip: any): any => {
-	// IPV6 addresses can include IPV4 addresses
-	// So req.ip can be '::ffff:86.3.182.58'
-	// However geoip-lite returns null for these
 	console.log('rent');
 	if (ip.includes('::ffff:')) {
 		ip = ip.split(':').reverse()[0];
@@ -34,7 +31,6 @@ export const getIpInfoMiddleware = (
 	next: Function
 ) => {
 	const info = getIpInfo(req.ip);
-	console.log(info);
 	if (!info.error) {
 		req.ipInfo = info;
 	}
